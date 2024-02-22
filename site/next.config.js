43f47949b551a3f2db08987f997bf9fe8a8acf08
couldrons2022 <94409257+couldrons2022@users.noreply.github.com>
2024-02-22 12:05:00 +0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'couldron2 Technologies',
     description: 'The official store for Kasm supported workspaces.',
-    icon: 'https://github.com/couldrons2022/kasm-registry/img/logo.svg',
+    icon: 'https://github.com/couldrons2022/kasm-registry-1/img/logo.svg',
     listUrl: 'https://couldrons2022.github.io/kasm-registry-1/',
     contactUrl: 'https://github.com/couldrons2022/kasm-registry-1/issues',
   },
